bbcb616174e7fd2997bfd376d0c776f9aea296fb
dv <pavan.divi@outlook.com>
2022-12-11 16:39:00 +0530

    chore: wrong server target file path

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: 'vercel',
+  serverBuildTarget: "vercel",
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  server:
+    process.env.NODE_ENV === "development" ? undefined : "./app/server.js",
   routes: (defineRoutes) =>
     defineRoutes((route) => {
       route("/", "routes/comingSoon.tsx", {

a296f3f7a58cddc465d3d204635f25ae565734e3
dv <pavan.divi@outlook.com>
2022-12-11 16:17:39 +0530

    chore: implement vercel provider

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,16 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverBuildTarget: 'vercel',
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  routes: (defineRoutes) =>
+    defineRoutes((route) => {
+      route("/", "routes/comingSoon.tsx", {
+        index: true,
+      });
+    }),
 };
